9f2b3b3254f1839a976551b5afea3a6b1da3a1e4
Kinsey Ann Durham <kinseyanndurham@Kinseys-MacBook-Pro.local>
2013-05-08 15:33:30 -0600

    one commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DogParkFinder::Application.config.secret_token = 'ca09db2428cd8433a340873315c15b4270f3139da4aea468369cb8911d0eadfa0de48ab82813da5d45f9d39e387cb056096df7da5659a03a081b0741eb78e172'
