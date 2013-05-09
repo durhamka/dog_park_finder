fa509ed1473738c9501c293300530435d0ddb93a
Kinsey Ann Durham <kinseyanndurham@Kinseys-MacBook-Pro.local>
2013-05-09 16:12:05 -0600

    get very basic search by address working

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,5 @@
+class SearchController < ApplicationController
+  def search
+    puts "Foothills Dog Park" 
+  end  
+end
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 feature 'Search by Address' do
   scenario 'search nearby dog park by address' do
-    visit '/index'
+    visit '/'
     fill_in 'Where do you want to play', with: 'Boulder'
+    click_button "Search"
+    expect(page).to have_content("Foothills Dog Park")
+
   end
+  
 end
