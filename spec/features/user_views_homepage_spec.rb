2966e80ae27824909e64201e8fefb19c1fe8765d
Kinsey Durham <kinseyann505@gmail.com>
2013-08-30 11:11:40 -0600

    Add initial homepage styling

--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -4,5 +4,6 @@ feature 'View the homepage' do
   scenario 'user sees relevant information' do
     visit '/'
     expect(page.title).to eq('Dog Park Finder')
+    expect(page).to have_content('Find a dog park. Play with your best friend.')
   end
 end
