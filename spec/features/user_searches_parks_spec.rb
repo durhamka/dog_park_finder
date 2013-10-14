45f5da31f7c86b51413a87d372479aca4e4909d3
Kinsey Durham <kinseyann505@gmail.com>
2013-10-14 15:22:42 -0600

    Search results page renders correct message when no cities match

--- spec/features/user_searches_parks_spec.rb
+++ spec/features/user_searches_parks_spec.rb
@@ -12,4 +12,15 @@ feature 'User searches parks' do
     expect(page).to have_content('Expected Park')
     expect(page).not_to have_content('Unexpected Park')
   end
+
+  scenario 'with the name of a city for which no results are returned' do
+    FactoryGirl.create(:user_park, name: 'Expected Park', city: 'Denver')
+    FactoryGirl.create(:user_park, name: 'Unexpected Park', city: 'St Louis')
+
+    visit root_path
+    fill_in 'search_address', with: 'Miami'
+    click_button 'Search'
+
+    expect(page).to have_content('Sorry. There are no current dog parks that we know about in your city.')
+  end
 end
