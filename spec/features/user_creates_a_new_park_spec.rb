dd746555238bdd7691db4aef21563411daad8ad4
Kinsey Durham <kinseyann505@gmail.com>
2013-10-23 09:09:47 -0600

    Refactor of user creates park feature spec

--- spec/features/user_creates_a_new_park_spec.rb
+++ spec/features/user_creates_a_new_park_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
 
 feature 'User creates a new park' do
-  pending 'successfully' do
+  scenario 'successfully' do
     visit new_user_park_path
+    dog_park_creation_form
 
-    fill_in 'user_park_name', with: 'Harleigh Park'
-    fill_in 'user_park_address', with: '21 Pup St.'
-    fill_in 'user_park_city', with: 'Denver'
-    fill_in 'user_park_zip_code', with: '80201'
-    fill_in 'user_park_description', with: 'Water feature and doggie water fountain'
-    click_button 'Create Park'
+    expect(page).to have_content("Thank you for creating a park")
   end
 
   scenario 'with missing information, so it errors' do
@@ -18,5 +14,15 @@ feature 'User creates a new park' do
 
     expect(page).to have_content("City can't be blank")
     expect(page).to have_content("Name can't be blank")
+  end
+
+
+  def dog_park_creation_form
+    fill_in 'user_park_name', with: 'Harleigh Park'
+    fill_in 'user_park_address', with: '21 Pup St.'
+    fill_in 'user_park_city', with: 'Denver'
+    fill_in 'user_park_zip_code', with: '80201'
+    fill_in 'user_park_description', with: 'Water feature and doggie water fountain'
+    click_button 'Create Park'
   end
 end
--- spec/features/user_votes_on_a_park_spec.rb
+++ spec/features/user_votes_on_a_park_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 feature 'User Votes on a Park' do
   scenario 'with an upvote' do
-    FactoryGirl.create(:user_park)
-    visit user_parks_path
+    create_user_park
     click_button '+1'
 
     expect(current_path).to eq(user_parks_path)
@@ -13,14 +12,18 @@ feature 'User Votes on a Park' do
   end
 
   scenario 'with a downvote' do
-    FactoryGirl.create(:user_park)
-    visit user_parks_path
+    create_user_park
     click_button '-1'
 
     expect(current_path). to eq(user_parks_path)
     expect(page).to have_content('Thank you for your vote!')
     expect(page).to have_content('0 upvotes')
     expect(page).to have_content('1 downvote')
+  end
+
+  def create_user_park
+    FactoryGirl.create(:user_park)
+    visit user_parks_path
   end
 end
 
